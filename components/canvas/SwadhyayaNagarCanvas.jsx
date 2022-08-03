39f1150104e76a3b80ab2c6f34c193e25a044b65
HittuDesai <hitarth.desai@mail.utoronto.ca>
2022-08-03 18:09:26 +0530

    Added code for converting excel data into json data for creation of the map dataset

--- components/canvas/SwadhyayaNagarCanvas.jsx
+++ components/canvas/SwadhyayaNagarCanvas.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { Coordinate } from "../../utility/classes/Coordinate";
-import { PairOfCoordinates } from "../../utility/classes/PairOfCoordinates";
 import { Canvas } from "../base/Canvas";
 
 export const SwadhyayaNagarCanvas = () => {
@@ -10,11 +8,6 @@ export const SwadhyayaNagarCanvas = () => {
 	useEffect(() => {
 		const canvasElement = canvasRef.current;
 		setContext(canvasElement.getContext("2d"));
-
-		const one = new Coordinate(53.32055555555556, -1.7297222222222221);
-		const two = new Coordinate(53.31861111111111, -1.6997222222222223);
-		const pair = new PairOfCoordinates(one, two);
-		console.log(pair.distance());
 	}, []);
 
 	//todo Add calculations for width and height of canvas using the latitude and longitude bounds
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
+import * as XLSX from "xlsx";
 
 export default function Home() {
+	const [file, setFile] = useState(null);
+	useEffect(() => {
+		if (!file) return;
+		const reader = new FileReader();
+		reader.readAsArrayBuffer(file);
+		reader.onload = event => {
+			const binaryString = new Uint8Array(event.target.result);
+			const workbook = XLSX.read(binaryString, {
+				type: "array",
+				raw: true,
+				cellFormula: false,
+			});
+			const worksheetName = workbook.SheetNames[0];
+			const worksheetData = XLSX.utils.sheet_to_json(
+				workbook.Sheets[worksheetName]
+			);
+			const firstObject = worksheetData[2];
+			console.log(Object.values(firstObject));
+		};
+	}, [file]);
+
 	return (
 		<>
 			<Head>
@@ -10,6 +33,14 @@ export default function Home() {
 					content="Website for Rudra Developers"
 				/>
 			</Head>
+			<input
+				type="file"
+				accept="xls"
+				id="raised-button-file"
+				onChange={event => {
+					setFile(event.target.files[0]);
+				}}
+			/>
 		</>
 	);
 }
